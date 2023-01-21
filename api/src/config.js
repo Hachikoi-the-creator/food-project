eb25a58ae1d21c850d1df4442032857db2ab90dd
Hachikoi-the-creator <facka72@gmail.com>
2023-01-20 18:50:26 -0600

    fixed port issues

--- api/src/config.js
+++ api/src/config.js
@@ -1,17 +1,8 @@
-const {
-  CLOUD_USER,
-  CLOUD_PSW,
-  CLOUD_HOST,
-  CLOUD_PORT,
-  CLOUD_DB_NAME,
-  LOCAL_DB_PSW,
-} = process.env;
+const { CLOUD_USER, CLOUD_PSW, CLOUD_HOST, PORT, CLOUD_DB_NAME } = process.env;
 
 const USERNAME = CLOUD_USER || "postgres";
 const THE_PSW = CLOUD_PSW || LOCAL_DB_PSW;
-const HOST_AND_PORT = CLOUD_PORT
-  ? `${CLOUD_HOST}:${CLOUD_PORT}`
-  : "localhost:5432";
+const HOST_AND_PORT = PORT ? `${CLOUD_HOST}:${PORT}` : "localhost:5432";
 const DATABASE = CLOUD_DB_NAME || "food";
 
 const CONNECTION_STRING = `postgres://${USERNAME}:${THE_PSW}@${HOST_AND_PORT}/${DATABASE}`;
