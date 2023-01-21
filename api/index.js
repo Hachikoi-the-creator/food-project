052b024541b08c54e6d02d39b40cdb8fb5a28b73
Hachikoi-the-creator <facka72@gmail.com>
2023-01-20 19:34:10 -0600

    ports fixed frfr

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-const PORT = 1313;
+const PORT = process.env.PORT || 1313;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`-_-listening on localhost:${PORT}`); // eslint-disable-line no-console
   });
 });
-
-// TODO: ADD ANIME GIRL
