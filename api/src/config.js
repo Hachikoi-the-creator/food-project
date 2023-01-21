55a45860d8acfca79376fa9a48841814e4a20b7d
Hachikoi-the-creator <facka72@gmail.com>
2023-01-20 18:54:34 -0600

    fixed port issues V2

--- api/src/config.js
+++ api/src/config.js
@@ -2,7 +2,8 @@ const { CLOUD_USER, CLOUD_PSW, CLOUD_HOST, PORT, CLOUD_DB_NAME } = process.env;
 
 const USERNAME = CLOUD_USER || "postgres";
 const THE_PSW = CLOUD_PSW || LOCAL_DB_PSW;
-const HOST_AND_PORT = PORT ? `${CLOUD_HOST}:${PORT}` : "localhost:5432";
+// need to check another var, since linux has a PORT, prob
+const HOST_AND_PORT = CLOUD_USER ? `${CLOUD_HOST}:${PORT}` : "localhost:5432";
 const DATABASE = CLOUD_DB_NAME || "food";
 
 const CONNECTION_STRING = `postgres://${USERNAME}:${THE_PSW}@${HOST_AND_PORT}/${DATABASE}`;
