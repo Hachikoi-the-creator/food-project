bf838acbf3cdbf5376497b1968771a7111c1f87b
Hachikoi-the-creator <facka72@gmail.com>
2023-03-24 20:25:23 -0500

    colors changed

--- client/src/components/DietTypesSelector.jsx
+++ client/src/components/DietTypesSelector.jsx
@@ -2,35 +2,87 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { GenericFlexContainer } from "./styles/FilterStuff";
 import { filterByDietType } from "../redux/actions";
+import styled from "styled-components";
+
+const dietsa = [
+  "gluten free",
+  "ketogenic",
+  "vegetarian",
+  "lacto-vegetarian",
+  "ovo-vegetarian",
+  "egan",
+  "pescetarian",
+  "paleo",
+  "primal",
+  "low FODMAP",
+  "whole30",
+];
+
+const DietsFilter = styled.div`
+  p {
+    text-align: center;
+  }
+
+  .btns-container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 0.6rem;
+
+    .active {
+      border: 2px solid var(--pink);
+    }
+  }
+`;
 
 export default function DietTypesSelector() {
   const allDiets = useSelector((state) => state.allDiets);
-  const [selectInput, setSelectInput] = useState("");
+  // const [selectInput, setSelectInput] = useState("");
+  const [currDiet, setcurrDiet] = useState("");
   const dispatcher = useDispatch();
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-
-    setSelectInput(value);
-    dispatcher(filterByDietType(value));
+  const handleChange = (dietType) => {
+    setcurrDiet(dietType);
+    dispatcher(filterByDietType(dietType));
   };
 
   return (
-    <div>
-      <GenericFlexContainer className="diet-selector">
-        <label>Filter by diets</label>
-
-        <select
-          onChange={handleChange}
-          defaultValue={selectInput.length ? selectInput : "all"}
-        >
-          {allDiets.map((diet) => (
-            <option key={diet.id} value={diet.dietName}>
-              {diet.dietName}
-            </option>
+    <GenericFlexContainer>
+      <DietsFilter>
+        <p>Filter by Diets</p>
+        <div className="btns-container">
+          {dietsa.map((diet) => (
+            <button
+              key={diet}
+              onClick={() => handleChange(diet)}
+              className={`${diet === currDiet && "active"}`}
+            >
+              {diet}
+            </button>
           ))}
-        </select>
-      </GenericFlexContainer>
-    </div>
+        </div>
+      </DietsFilter>
+    </GenericFlexContainer>
   );
+  // return (
+  //   <div>
+  //     <GenericFlexContainer className="diet-selector">
+  //       <label>Filter by diets</label>
+
+  //       <select
+  //         onChange={handleChange}
+  //         defaultValue={selectInput.length ? selectInput : "all"}
+  //       >
+  //         {allDiets.map((diet) => (
+  //           <option key={diet.id} value={diet.dietName}>
+  //             {diet.dietName}
+  //           </option>
+  //         ))}
+  //       </select>
+  //     </GenericFlexContainer>
+  //   </div>
+  // );
 }
--- readme.md
+++ readme.md
@@ -4,4 +4,10 @@
 - Render
 
 - Link to emm stuff yeah...
-https://docs.google.com/spreadsheets/d/1BbyjtdHcB-2ewupMVziJLFxgYjUoK688N9-JodkGTCA/edit#gid=0
+  https://docs.google.com/spreadsheets/d/1BbyjtdHcB-2ewupMVziJLFxgYjUoK688N9-JodkGTCA/edit#gid=0
+
+# Credits
+
+### chef img
+
+Photo by <a href="https://unsplash.com/@johnathanmphoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Johnathan Macedo</a> on <a href="https://unsplash.com/s/photos/chef?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
